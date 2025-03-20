5ee941cfa54293e3d27e97aadf4070c5b6ca21ff
F3lip3Jara <32960462+F3lip3Jara@users.noreply.github.com>
2025-03-20 10:18:14 -0300

    Update environment.service.ts

--- src/app/erp/service/environment.service.ts
+++ src/app/erp/service/environment.service.ts
@@ -5,11 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class Environment {
     public cluster     = 'mt1';
-    public key         = 'ASDbO1OD8RIa8C37Ox';
-    public wsHost      = 'app.back.agileti.cl';
-    public wsPort      = '6001';
+    public key         = '';
+    public wsHost      = '';
+    public wsPort      = '';
     public forceTLS    = 'false';
     public disableStats= 'false';
     public keygoogle   = "";
-    public keygoogleMap= 'AIzaSyADuggTVu7JnRb0L_fIENMWltBUgHBCSkI';
+    public keygoogleMap= '';
 } 
